98cca973690da1e95793331ea6a9e7489762b043
ldk-hub <orm6711@gmail.com>
2022-12-30 22:50:48 +0900

    feat : frontend, vue 연동

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: "../src/main/resources/static/vue",  // vue를 빌드 한 경로지정
+  indexPath: "../static/index.html",    // vue의 SPA를 보여줄 페이지 경로 지정
+  devServer: {
+    proxy: 'http://localhost:9091' // 백엔드 서버와 프록시를통해 데이터를 주고받음.
+  }
 })
